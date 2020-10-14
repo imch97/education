73b0712ab30b3c07b3cdf29474b6498533463736
Ivan_WEB <ivan.exceedteam@gmail.com>
2020-10-14 14:52:18 +0300

    Complete task 3

--- 1. Basic/3) basic-calc/index.js
+++ 1. Basic/3) basic-calc/index.js
@@ -1,5 +1,27 @@
 function basicCalc(operation, a, b) {
-  // Напиши свой код здесь
+  //let choice;
+  let asnwer;
+  switch(operation){
+    case"+":
+    asnwer=a+b;
+    break;
+
+    case"-":
+    asnwer=a-b;
+    break;
+    
+    case"*":
+    asnwer=a*b;
+    break;
+
+    case"/":
+    asnwer=a/b;
+    break;
+    
+    default:break;
+  }
+
+  return asnwer;
 }
 
 window.basicCalc = basicCalc;
