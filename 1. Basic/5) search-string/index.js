8e7205f3de221315c14fdef1ed7ca9778248b33e
Ivan_WEB <ivan.exceedteam@gmail.com>
2020-10-14 15:47:53 +0300

    Complete task 5

--- 1. Basic/5) search-string/index.js
+++ 1. Basic/5) search-string/index.js
@@ -1,5 +1,24 @@
 function searchString(obj) {
-  // Напиши свой код здесь
+  let s;
+  let endS="?";
+  for(let key in obj){
+    s=""
+    if ((obj[key]===null)||(obj[key]===undefined)||(obj[key]==="")) continue ;
+    s=String(""+s+String(key)+"="+String(obj[key])+"&");
+    endS=endS+s;
+  }
+
+
+//toBe('?a=123&b=qwe')
+  // for (let key in user) {
+  //   // ключи
+  //   alert( key );  // name, age, isAdmin
+  //   // значения ключей
+  //   alert( user[key] ); // John, 30, true
+  // }
+
+  //str.substring(0, str.length - 1)
+return endS.substring(0,endS.length-1);
 }
 
 window.searchString = searchString;
