68da038585a25b0e6c1d648436286d64c0c71679
Ivan_WEB <ivan.exceedteam@gmail.com>
2020-10-15 17:42:03 +0300

    Continue tasl 3_3

--- 3. Array/3) arrays-sort/index.js
+++ 3. Array/3) arrays-sort/index.js
@@ -1,5 +1,27 @@
 function arraysSort(arr) {
-  // Напиши свой код здесь
+  
+  //Math.max(...args)
+  // let  max;
+
+  //  for(let i=0;i<(arr.lenght);i++){
+  //     max=Math.max(...arr[i]);
+      
+  //  } 
+  // return c;
+
+  let c = arr.sort(function(a,b) {
+    
+    if ((a[1]!=null)&&(a[1]!=null)) return a[1] - b[1];
+    if ((a[1]==null)&&(a[1]==null)) return a - b;
+    
+    //return a - b;
+    //return a[1] - b[1];
+    //return a[Math.max(1)] - b[Math.max(1)];
+
+  });
+
+
+return c;
 };
 
 window.arraysSort = arraysSort;
