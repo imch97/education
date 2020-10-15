9ca40489d0cc7d689a862c920029784e9abaa80b
Ivan_WEB <ivan.exceedteam@gmail.com>
2020-10-15 12:17:38 +0300

    Complete task 2_3

--- 2. Data Types/3) is-equal-simple/index.js
+++ 2. Data Types/3) is-equal-simple/index.js
@@ -1,5 +1,29 @@
 function isEqual(a, b) {
-  // Напиши свой код здесь
+
+  let s;
+  let endS1="";
+  let endS2="";
+
+  if (a==null||b==null) return false;
+
+  for(let key in a){
+    s=""
+    s=String(""+s+String(key)+"="+String(a[key])+"&");
+    endS1=endS1+s;
+  }
+  endS1=endS1.substring(0,endS1.length-1)
+
+
+  for(let key in b){
+    s="" 
+    s=String(""+s+String(key)+"="+String(b[key])+"&");
+    endS2=endS2+s;
+  }
+  endS2=endS2.substring(0,endS2.length-1)
+  
+  return endS1===endS2;
+
+
 }
 
 window.isEqual = isEqual;
