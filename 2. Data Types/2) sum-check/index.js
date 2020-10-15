462b75d1e8d47f4943bbbbd0fb5ef5015f2e4e14
Ivan_WEB <ivan.exceedteam@gmail.com>
2020-10-15 11:49:41 +0300

    Complete task 2_2

--- 2. Data Types/2) sum-check/index.js
+++ 2. Data Types/2) sum-check/index.js
@@ -1,5 +1,14 @@
 function sumCheck(a, b) {
-  // Напиши свой код здесь
+  let er= "Wrong arguments type!";
+  let isValidA = typeof a === 'number';
+  let isValidB = typeof b === 'number';
+
+
+   if(isValidA && isValidB){
+      return a+b;
+    }
+  throw er;
+
 }
 
 window.sumCheck = sumCheck;
