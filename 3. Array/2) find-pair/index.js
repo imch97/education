f395505b55e859c5d8314befc529858a23225c94
Ivan_WEB <ivan.exceedteam@gmail.com>
2020-10-15 16:22:27 +0300

    Complete task 3_2

--- 3. Array/2) find-pair/index.js
+++ 3. Array/2) find-pair/index.js
@@ -1,5 +1,22 @@
 function findPair(arr) {
-  // Напиши свой код здесь
+  
+  let count=0;
+  let l =arr.length;
+
+
+  for(let i = 0 ; i < (l-1); i++){
+
+      for(let j = i + 1;j < l ; j++){
+        
+        if (arr[i] === arr[j]) {
+          count++;
+          return arr[i];          
+        }
+        
+      }
+
+  }
+  if (count==0) return null;
 };
 
 window.findPair = findPair;
