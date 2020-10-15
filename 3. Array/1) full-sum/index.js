1c6dcf7e574d21f5b110194cda9d3e4b98aa3cde
Ivan_WEB <ivan.exceedteam@gmail.com>
2020-10-15 15:38:27 +0300

    Complete task 3_1

--- 3. Array/1) full-sum/index.js
+++ 3. Array/1) full-sum/index.js
@@ -1,5 +1,14 @@
 function fullSum(...args) {
-  // Напиши свой код здесь
+  let er="Wrong Argument Type";
+  let s=0;
+  if (!Array.isArray(args))  throw er;
+
+  for (let i = 0; i < args.length; i++) {
+    if (typeof args[i]!=="number") throw er;
+    s=s+args[i];    
+  }
+
+  return s;
 }
 
 window.fullSum = fullSum;
