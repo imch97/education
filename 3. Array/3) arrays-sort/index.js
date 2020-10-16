6010202e33e1373438c33b576a16282bcb6a2e68
Ivan_WEB <ivan.exceedteam@gmail.com>
2020-10-16 10:32:26 +0300

    Complete task 3_3

--- 3. Array/3) arrays-sort/index.js
+++ 3. Array/3) arrays-sort/index.js
@@ -1,28 +1,35 @@
 function arraysSort(arr) {
   
-  //Math.max(...args)
-  // let  max;
 
-  //  for(let i=0;i<(arr.lenght);i++){
-  //     max=Math.max(...arr[i]);
-      
-  //  } 
-  // return c;
-
+  /* ПОЧТИ РАБОТАЕТ
   let c = arr.sort(function(a,b) {
     
     if ((a[1]!=null)&&(a[1]!=null)) return a[1] - b[1];
     if ((a[1]==null)&&(a[1]==null)) return a - b;
-    
-    //return a - b;
-    //return a[1] - b[1];
-    //return a[Math.max(1)] - b[Math.max(1)];
-
+        
   });
 
 
-return c;
-};
+return c;*/
+
+    let n=arr.length;
+    let tmp; 
+    
+    for (let ii = 0;ii<n;ii++) {
+      for (let i=0; i<n-1; i++ ) {                
+        if (Math.max(...arr[i])>Math.max(...arr[i+1])) {                
+                  tmp=arr[i]; arr[i]=arr[i+1]; arr[i+1]=tmp; 
+                  }
+      }
+  }
+
+  let newArr = [];
+
+  for(let i = 0; i<n;i++){
+      newArr.push(arr[i]);
+  } 
+    return newArr;
+  };
 
 window.arraysSort = arraysSort;
 
