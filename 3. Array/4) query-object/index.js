875ce4c0b4fea4486566b290fd5fe2a119e316a1
Ivan_WEB <ivan.exceedteam@gmail.com>
2020-10-16 13:18:20 +0300

    Complete task 3_4

--- 3. Array/4) query-object/index.js
+++ 3. Array/4) query-object/index.js
@@ -1,5 +1,36 @@
+function stringToType(str) {
+  let type =typeof(str);
+  if (str==="") return str;
+  if (str.isNaN) return NaN;
+  if (str=="null") return null;
+  if (str=="true") return true;
+  if (str=="false") return false;
+  if (str=="undefined") return undefined;
+  if (isNaN(str)) return str;
+  if (!isNaN(str)) return +str;
+};
+
+
 function queryToObject(query) {
-  // Напиши свой код здесь
+  if (query=="") return {};
+  let m = [];
+  query = query.substr(1,query.length);
+  if (query=="") return {};
+  m = query.split("&");
+
+  for(let i = 0; i<m.length;i++){
+    m[i]=m[i].split("=");
+  }
+
+  for(let i = 0; i < m.length; i++){
+    m[i][1] = stringToType(m[i][1]);
+  }
+
+  let obj = Object.fromEntries(m);
+  
+  console.log(obj)
+  return obj;
+ 
 };
 
 window.queryToObject = queryToObject;
