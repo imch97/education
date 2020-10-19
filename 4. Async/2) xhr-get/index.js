ddea62acf9b61e7bb2849fce513c17ca5c11542e
Ivan_WEB <ivan.exceedteam@gmail.com>
2020-10-19 13:06:54 +0300

    Complete task 4_2

--- 4. Async/2) xhr-get/index.js
+++ 4. Async/2) xhr-get/index.js
@@ -1,5 +1,22 @@
 function xhrGet(url) {
-  // Напиши свой код здесь
+  
+
+let xhr = new XMLHttpRequest();
+
+
+xhr.open('GET', url, false);
+xhr.setRequestHeader('Content-Type', 'application/json');
+xhr.responseType = 'json';
+xhr.send();
+
+
+return new Promise ((resolve, reject) => {
+  if (xhr.status >=200 && xhr.status <=299) {
+    return resolve(xhr.response);
+  }  
+  return reject(xhr.response);
+});
+ 
 }
 
 window.xhrGet = xhrGet;
