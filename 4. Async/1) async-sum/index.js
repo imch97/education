80ddbb548ca1ef89715186c543ac69de4772f78f
Ivan_WEB <ivan.exceedteam@gmail.com>
2020-10-19 11:20:33 +0300

    Complete task 4_1

--- 4. Async/1) async-sum/index.js
+++ 4. Async/1) async-sum/index.js
@@ -1,7 +1,10 @@
 function asyncSum(a, b) {
-  
-  
-  
+  return new Promise((resolve, reject) => {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return reject(new Error('not a number'))
+    }
+    resolve(a+b)
+  })
 }
 
 window.asyncSum = asyncSum;
