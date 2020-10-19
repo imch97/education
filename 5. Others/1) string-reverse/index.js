9491819d904eab82d44e1e28463615b357b0cd77
Ivan_WEB <ivan.exceedteam@gmail.com>
2020-10-19 16:04:13 +0300

    Complete task 5_1

--- 5. Others/1) string-reverse/index.js
+++ 5. Others/1) string-reverse/index.js
@@ -6,3 +6,16 @@
   'bob'.reverse() === 'bob';
   'abc123'.reverse() === '321cba';
  */
+function perevorot(str){
+  if ( ! str ) { str = this; }
+  let nstr = "";
+  console.log(str);
+  console.log(str.length);
+    for (let i = str.length - 1; i >= 0; i--) {
+        nstr += str[i];
+    }
+    console.log("before: "+str+" after: " + nstr);
+    return nstr;
+}
+
+String.prototype.reverse = perevorot;
