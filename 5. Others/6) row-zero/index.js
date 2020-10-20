71604f69aa6685f3bb5bdda78e18c38f960a36a6
Ivan_WEB <ivan.exceedteam@gmail.com>
2020-10-20 11:54:28 +0300

    Complete task 5_6

--- 5. Others/6) row-zero/index.js
+++ 5. Others/6) row-zero/index.js
@@ -1,5 +1,18 @@
+import { formatURL } from "xhr-mock/lib/MockURL";
+
 function rowZero(str) {
-  // Напиши свой код здесь
+  let max_count_zero=0;
+  let itog=0;
+  let l = str.length;
+  for(let i = 0; i < str.length;i++){
+    if(str[i]==="0") {max_count_zero++; };
+    if (itog<max_count_zero) {itog=max_count_zero;}
+    if( str[i]==="1") {max_count_zero=0;continue;}
+  }
+
+
+
+return itog;
 }
 
 window.rowZero = rowZero;
